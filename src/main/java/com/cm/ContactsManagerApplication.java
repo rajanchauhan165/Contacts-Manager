f96244d48b71a818afd81895ea8b204f53806d7e
rajan chauhan <rajanchauhan165@gmail.com>
2023-01-13 19:47:09 +0530

    dependencies added

---
+++ src/main/java/com/cm/ContactsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsManagerApplication.class, args);
+	}
+
+}
